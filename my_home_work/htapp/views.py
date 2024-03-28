bd8a109ab58332810517c61f94f803781e9efa62
Denis Burguto <6545158@gmail.com>
2024-03-28 09:07:31 +0300

    logs rules managed

--- my_home_work/htapp/views.py
+++ my_home_work/htapp/views.py
@@ -1,6 +1,4 @@
 import logging
-
-from django.shortcuts import render
 from django.http import HttpResponse
 
 logger = logging.getLogger(__name__)
@@ -13,6 +11,7 @@ about_html = """
 <h1>ABOUT<h1/>
 """
 
+
 def main(request):
     logger.info('Main page accessed')
     return HttpResponse(main_html)
@@ -23,4 +22,3 @@ def about(request):
     return HttpResponse(about_html)
 
 # Create your views here.
-

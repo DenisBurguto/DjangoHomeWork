271a8f22ae06d65b1df13e17aedd36bad31d31ad
Denis Burguto <65451582GMAIL.COM>
2024-04-04 12:31:56 +0300

    date filtering and get only unique products

--- my_home_work/htapp3/urls.py
+++ my_home_work/htapp3/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('get-orders-by-client/', views.get_orders_by_client, name='get_orders_by_client'),
     path('all-client-orders/<int:client_id>/', views.all_client_orders, name='all_client_orders'),
+    path('all-client-orders/<int:client_id>/<int:period>', views.all_client_orders, name='all_client_orders'),
+
     # path('products/', views.products, name='products'),
     # path('orders/', views.orders, name='orders')
 ]
--- my_home_work/htapp3/views.py
+++ my_home_work/htapp3/views.py
@@ -1,3 +1,5 @@
+from datetime import datetime, timedelta
+
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
@@ -15,15 +17,27 @@ def about(request):
 def get_orders_by_client(request):
     if request.method == 'POST':
         client_id = request.POST.get('client_id')
-        if client_id:
-            return HttpResponseRedirect(reverse('all_client_orders', args=[client_id]))
+        period = request.POST.get('period')
+        unique = request.POST.get('unique')
+        kwargs = {'client_id': client_id}
+        if period:
+            kwargs['period'] = period
+        return HttpResponseRedirect(reverse('all_client_orders', kwargs=kwargs))
     return render(request, 'htapp3/get_orders_by_client.html')
 
 
-def all_client_orders(request, client_id):
+def all_client_orders(request, **kwargs):
+    client_id = kwargs.get('client_id')
+    period = kwargs.get('period')
     client = get_object_or_404(Client, pk=client_id)
-    orders = Order.objects.filter(customer=client)
-    products = Product.objects.filter(order__customer=client)
-    return render(request, 'htapp3/all_client_orders.html', {'client': client, 'orders': orders, 'products': products})
+    if period:
+        end_date = datetime.now()
+        start_date = end_date - timedelta(days=int(period))
+        orders = Order.objects.filter(customer=client, date_ordered__gte=start_date, date_ordered__lte=end_date)
+        products = Product.objects.filter(order__customer=client).distinct()
+    else:
+        orders = Order.objects.filter(customer=client)
+        products = Product.objects.filter(order__customer=client)
+    return render(request, 'htapp3/all_client_orders.html', {'period': period, 'client': client, 'orders': orders, 'products': products})
 
 # Create your views here.

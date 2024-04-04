e76f7194c6bef3aeaec64449e1867596017693bb
Denis Burguto <65451582GMAIL.COM>
2024-04-04 11:26:50 +0300

    all orders by client

--- my_home_work/htapp3/urls.py
+++ my_home_work/htapp3/urls.py
@@ -4,7 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.main, name='index'),
     path('about/', views.about, name='about'),
-    # path('clients/', views.clients, name='clients'),
+    path('get-orders-by-client/', views.get_orders_by_client, name='get_orders_by_client'),
+    path('all-client-orders/<int:client_id>/', views.all_client_orders, name='all_client_orders'),
     # path('products/', views.products, name='products'),
     # path('orders/', views.orders, name='orders')
 ]
--- my_home_work/htapp3/views.py
+++ my_home_work/htapp3/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
+from htapp2.models import Client, Order, Product
 
 
 def main(request):
@@ -7,5 +10,20 @@ def main(request):
 
 def about(request):
     return render(request, "htapp3/about.html")
+
+
+def get_orders_by_client(request):
+    if request.method == 'POST':
+        client_id = request.POST.get('client_id')
+        if client_id:
+            return HttpResponseRedirect(reverse('all_client_orders', args=[client_id]))
+    return render(request, 'htapp3/get_orders_by_client.html')
+
+
+def all_client_orders(request, client_id):
+    client = get_object_or_404(Client, pk=client_id)
+    orders = Order.objects.filter(customer=client)
+    products = Product.objects.filter(order__customer=client)
+    return render(request, 'htapp3/all_client_orders.html', {'client': client, 'orders': orders, 'products': products})
 
 # Create your views here.

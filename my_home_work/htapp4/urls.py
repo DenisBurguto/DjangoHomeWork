f4df7195a38a313d44355c858dfd454508a7bb60
Denis Burguto <65451582GMAIL.COM>
2024-04-09 06:46:59 +0300

    product create & product upd

--- my_home_work/htapp4/urls.py
+++ my_home_work/htapp4/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('get-product-by-id/', views.get_product_by_id, name='get_product_by_id'),
     path('get-product-by-id/<str:success_message>', views.get_product_by_id, name='get_product_by_id'),
     path('product-update/<int:product_id>/', views.product_update, name='product_update'),
+    path('product-create/', views.product_create, name='product_create'),
 ]
--- my_home_work/htapp4/views.py
+++ my_home_work/htapp4/views.py
@@ -1,4 +1,3 @@
-
 from django.shortcuts import render, redirect
 from django.urls import reverse
 
@@ -6,7 +5,7 @@ from htapp2.models import Product
 from htapp4.forms import ProductForm
 
 
-def get_product_by_id(request,success_message=None):
+def get_product_by_id(request, success_message=None):
     if request.method == 'POST':
         product_id = request.POST.get('product_id')
         return redirect(reverse('product_update', args=(product_id,)))
@@ -19,7 +18,7 @@ def product_update(request, product_id):
         form = ProductForm(request.POST, instance=product)
         if form.is_valid():
             form.save()
-            success_message=f"Product {product_id} successfully updated."
+            success_message = f"Product with ID {product_id} successfully updated."
             return redirect('get_product_by_id', success_message=success_message)
     else:
         form = ProductForm(instance=product)
@@ -27,6 +26,7 @@ def product_update(request, product_id):
 
 
 def product_create(request):
+    success_message = None
     if request.method == 'POST':
         form = ProductForm(request.POST)
         if form.is_valid():
@@ -36,9 +36,10 @@ def product_create(request):
             quantity = form.cleaned_data['quantity']
             new_product = Product(name=name, description=description, price=price, quantity=quantity)
             new_product.save()
-    else:
-        form = ProductForm()
+            success_message = f"Product successfully created with ID {new_product.pk}."
+
+    form = ProductForm()
     return render(request, 'htapp4/product_create.html',
-                  {'form': form})
+                  {'form': form, 'success_message': success_message})
 
 # Create your views here.

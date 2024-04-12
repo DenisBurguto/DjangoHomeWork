5c650703036b7be50a9deec174d20677ee3eb76b
Denis Burguto <65451582GMAIL.COM>
2024-04-12 05:29:00 +0300

    admin : product representation

--- my_home_work/htapp2/admin.py
+++ my_home_work/htapp2/admin.py
@@ -1,8 +1,40 @@
+from decimal import Decimal
+
 from django.contrib import admin
 from .models import Client, Product, Order
 
-admin.site.register(Product)
-admin.site.register(Order)
+DISCOUNT = 10
+
+
+@admin.action(description="apply  discount")
+def apply_discount(modeladmin, request, queryset):
+    for product in queryset:
+        product.price -= (product.price * Decimal(DISCOUNT / 100))
+        product.save()
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price', 'quantity', 'photo']
+    ordering = ['-quantity']
+    list_filter = ['created_at', 'price', 'quantity']
+    search_fields = ['description', 'name']
+    search_help_text = 'seach in names and descriptions'
+    actions = [apply_discount]
+    readonly_fields = ['created_at']
+    fieldsets = [
+        ('main', {'classes': ['wide'], 'fields': ['name', 'price', 'quantity']}),
+        ('description', {'classes': ['collapse'], 'fields': ['description', 'photo']}),
+        ('created_at', {'fields': ['created_at']}),
+    ]
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['customer','total_amount', 'date_ordered']
+    ordering=['-date_ordered', '-total_amount',]
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Order, OrderAdmin)
 admin.site.register(Client)
 
 # Register your models here.
